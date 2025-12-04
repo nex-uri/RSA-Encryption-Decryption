f41a70ad882af644c9a116d54933575b5f2dce32
nex-uri <wdtnjp27d9@privaterelay.appleid.com>
2025-12-04 21:54:57 +0800

    Added "message_value.cs" class.

---
+++ RSA Encryption & Decryption/message_value.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSA_Encryption___Decryption
+{
+    internal class message_value
+    {
+    }
+}
